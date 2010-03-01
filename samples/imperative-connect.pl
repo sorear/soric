fcfa6dfaae0dc496dce43b92cb31791603ddafde
Stefan O'Rear <stefanor@cox.net>
2010-02-28 18:14:10 -0800

    Work around a MooseX::Declare bug for now

--- samples/imperative-connect.pl
+++ samples/imperative-connect.pl
@@ -4,18 +4,19 @@ use MooseX::Declare;
 use Soric3::Kernel;
 use AnyEvent;
 
-my $kernel = Soric3::Kernel->new(client_class =>
-    class extends Soric3::Module with Soric3::Role::SendQueue {
-        method requires($class:) {
-            'Irc'
-        }
+my $client = class extends Soric3::Module with Soric3::Role::SendQueue {
+    method requires($class:) {
+        'Irc'
+    }
 
-        method BUILD() {
-            $self->module('Irc')->new_connection(
-                tag => 'freenode', nickname => 'soric-test',
-                host => 'irc.freenode.net');
-            $self->queue_send(freenode => [PRIVMSG => sorear => 'Hello!']);
-        }
-    });
+    method BUILD() {
+        $self->module('Irc')->new_connection(
+            tag => 'freenode', nickname => 'soric-test',
+            host => 'irc.freenode.net');
+        $self->queue_send(freenode => [PRIVMSG => sorear => 'Hello!']);
+    }
+};
+
+my $kernel = Soric3::Kernel->new(client_class => $client);
 
 AnyEvent->cond_var->recv;
