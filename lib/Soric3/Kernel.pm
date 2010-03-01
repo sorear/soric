f1e670065932d20eac5f92700f70b75082a87be0
Stefan O'Rear <stefanor@cox.net>
2010-02-28 16:42:12 -0800

    New, much simpler module load system

--- lib/Soric3/Kernel.pm
+++ lib/Soric3/Kernel.pm
@@ -1,31 +1,13 @@
 use MooseX::Declare;
 
-# TODO do something for the distinguished client module, also implement the
-# role-based broadcast mechanism
-#
-# for broadcast the kernel needs to consider itself as a module... this is
-# for configuration changes, and also for receiving module_req_changed
+# TODO implement role-based broadcast
+# TODO We need to do something to allow modules to change their depends at
+# will, especially for UI clients and configuration
 
-class Soric3::Kernel does Soric3::Meta::Alertable {
-    has _config_delegate => (
-        isa => 'Soric3::ConfigDelegate',
-        is  => 'ro',
-        required => 1,
-        handles => { set_config => 'set',
-                     get_config => 'get' },
-    );
+# the configuration logic should be hard-coded, kept in a separate module
+# with delegation... XXX investigate possible Moose models, study Kioku/DBIC
 
-    method BUILDARGS ($class: @args) {
-        die "You must pass 1 or an even number of arguments"
-            unless @args == 1 || (@args % 2) == 0;
-
-        if (@args == 1) {
-            return {_config_delegate => $args[0]};
-        } else {
-            return {_config_delegate => Soric3::HashConfigDelegate->new(@args)};
-        }
-    }
-
+class Soric3::Kernel with Soric3::Meta::Alertable {
     has _modules => (
         isa     => 'HashRef[Soric3::Module]',
         is      => 'bare',
@@ -38,47 +20,41 @@ class Soric3::Kernel does Soric3::Meta::Alertable {
     );
 
     # Do a preorder traversal of the module dependancy tree, instantiating and
-    # binding all new modules, and removing unused ones.  This is done from an
-    # idle handler to preserve the property that configuration changes are
-    # nonfinal before returning to the main loop.
-    method _load_module(HashRef $used, Str $name where { $_ =~ /^[a-z]+$/) {
+    # binding all new modules, and removing unused ones.
+    method _load_module(HashRef $used, Str $name where { $_ =~ /^[a-z]+$/ }) {
         return if $used->{$name};
 
-        my $class_name = "Soric3::Module::" . uc($name);
+        my $class_name = "Soric3::Module::" . $name;
         Class::MOP::load_class($class_name);
 
         $used->{$name} = 1;
 
-        $self->_load_module($used, $_) for $name->requires;
+        # XXX we ought to be doing something dynamic here
+        $self->_load_module($used, $_) for $class_name->requires;
 
+        return if $self->module($name);
         $self->_bind_module($name, $class_name->new(kernel => $self));
     }
 
-    method _react() {
+    method _install_modules() {
         my %used;
 
+        $self->_bind_module(CLIENT =>
+            $self->client_class->new_object(kernel => $self))
+                unless defined $self->module('CLIENT');
+
         $self->_load_module(\%used, $_)
-            for ($self->_config_delegate->keys());
+            for ($self->client_class->name->requires);
 
         for my $mod ($self->loaded_modules) {
             next if $used{$mod};
+            next if $mod eq 'CLIENT';
 
             $self->_unload_module($mod);
         }
     }
 
-    after set_config => sub {
-        my ($self, @path) = @_;
-
-        for my $mod (@path ? $path[0] : $self->loaded_modules) {
-            next unless $self->module_loaded($mod);
-            $self->module($mod)->config_changed(@path);
-        }
-
-        $self->_alert;
-    };
-
     method BUILD {
-        $self->_alert;
+        $self->_install_modules;
     }
 }
